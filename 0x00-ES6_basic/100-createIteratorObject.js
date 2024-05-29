9491bbcb23c2f0f42cb109751627780ab320d2db
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-05-30 02:52:40 +0300

    "hotfix"

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,20 +1,9 @@
 export default function createIteratorObject(report) {
-  return {
-    [Symbol.iterator]() {
-      const employees = [];
-      for (const department in report.allEmployees) {
-        employees.push(...report.allEmployees[department]);
-      }
-
-      let currIdx = 0;
-      return {
-        next() {
-          if (currIdx < employees.length)
-            return { value: employees[currIdx++], done: false };
-          else
-            return {done: true};
-        }
+  return (function* _() {
+    for (const department of Object.values(report.allEmployees)) {
+      for (const employee of department) {
+        yield employee;
       }
     }
-  };
+  }());
 }
