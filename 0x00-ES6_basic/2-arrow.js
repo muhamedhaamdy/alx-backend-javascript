d96dcf32f2e1aa1f49062576f493a0280be3c96b
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-05-29 23:15:28 +0300

    fix Arrow functions

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -2,10 +2,9 @@ const getNeighborhoodsList = function () {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
   const self = this;
-  const add = (newNeighborhood) => {
+  this.addNeighborhood = (newNeighborhood) => {
     self.sanFranciscoNeighborhoods.push(newNeighborhood);
     return self.sanFranciscoNeighborhoods;
   };
-  this.addNeighborhood = add;
 };
 export default getNeighborhoodsList;
