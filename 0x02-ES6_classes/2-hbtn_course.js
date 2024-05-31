6ef51d28c5b9a5b56e4c2a7aab3c00346acae282
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-05-31 04:37:25 +0300

    A Course, Getters, and Setters

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -8,13 +8,14 @@ export default class HolbertonCourse {
   get name() {
     return this._name;
   }
+
   get length() {
     return this._lenght;
   }
+
   get students() {
     return this._students;
   }
-
 
   set name(value) {
     if (typeof value !== 'string') {
@@ -22,12 +23,14 @@ export default class HolbertonCourse {
     }
     this._name = value;
   }
+
   set length(value) {
     if (typeof value !== 'number') {
       throw new TypeError('Length must be a number');
     }
     this._lenght = value;
   }
+
   set students(value) {
     if (!Array.isArray(value)) {
       throw new TypeError('Students must be an array of strings');
