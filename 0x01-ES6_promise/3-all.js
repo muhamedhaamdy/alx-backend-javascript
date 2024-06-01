c58208ad713fe4f4a436f26e30461de9b4cde77f
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-06-01 04:49:39 +0300

    Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,15 +1,10 @@
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup () {
-  uploadPhoto().then((photo) => {
-    return photo.body;
-  }).then((body) => {
-    createUser().then((user) => {
-      console.log(`${body} ${user.firstName} ${user.lastName}`);
-    }).catch(() => {
-      console.log('signup system offline');
-    });
-  }).catch(() => {
-    console.log('Signup system offline');
-  });
+export default function handleProfileSignup() {
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
 }
