865143e81d916f986e3ae77251c582498b436823
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-06-04 09:30:25 +0300

    More map data structur

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,9 @@
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  for (const [key, val] of map) {
+    if (val === 1) map.set(key, 100);
+  }
+}
