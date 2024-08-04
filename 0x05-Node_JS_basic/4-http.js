71691b2f24b5fa78042b0b5324dd68f3d1499143
Your Name <mmhamdyyy@gmail.com>
2024-08-04 21:11:16 +0300

    Create a small HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,7 +1,20 @@
-const app = require('http');
+const http = require('http');
 
-app.createServer((req, res) => {
-  res.end('Hello Holberton School!');
-}).listen(1245);
+const PORT = 1245;
+const HOST = 'localhost';
+const app = http.createServer();
+
+app.on('request', (_, res) => {
+  const responseText = 'Hello Holberton School!';
+
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', responseText.length);
+  res.statusCode = 200;
+  res.write(Buffer.from(responseText));
+});
+
+app.listen(PORT, HOST, () => {
+  process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
+});
 
 module.exports = app;
--- 0x05-Node_JS_basic/main.js
+++ 0x05-Node_JS_basic/main.js
@@ -1,10 +1,19 @@
-const countStudents = require('./3-read_file_async');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
 
-countStudents("database.csv")
-    .then(() => {
-        console.log("Done!");
-    })
-        .catch((error) => {
-        console.log(error);
-    });
-console.log("After!");
+const app = require('./4-http');
+
+chai.use(chaiHttp);
+chai.should();
+
+describe('Small HTTP server', () => {
+  it('Returns the right content for /', (done) => {
+    chai.request(app)
+      .get('/')
+      .end((error, response) => {
+        chai.expect(response.text).to.equal('Hello Holberton School!');
+        chai.expect(response.statusCode).to.equal(200);
+        done();
+      });
+  });
+});
